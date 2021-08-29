474a4fc3e3ce8458fc2729a4b6a896e28de934ac
Jordevá <jordevalucas@gmail.com>
2021-08-29 15:40:48 -0300

    fix: corrige lint

--- frontend/src/components/ToastPortal/ToastPortal.js
+++ frontend/src/components/ToastPortal/ToastPortal.js
@@ -1,31 +1,31 @@
-import { uuid } from '../../shared';
-import ReactDOM from 'react-dom';
-import { Toast } from '../../components';
-import styles from './styles.module.css';
-import { useToastPortal, useToastAutoClose } from '../../hooks';
-import { useState, forwardRef, useImperativeHandle } from 'react';
+import ReactDOM from 'react-dom'
+import { useState, forwardRef, useImperativeHandle } from 'react'
+import { uuid } from '../../shared'
+import { Toast } from '..'
+import styles from './styles.module.css'
+import { useToastPortal, useToastAutoClose } from '../../hooks'
 
 export const ToastPortal = forwardRef(
   ({ autoClose = false, autoCloseTime = 5000 }, ref) => {
-    const [toasts, setToasts] = useState([]);
-    const { loaded, portalId } = useToastPortal();
+    const [toasts, setToasts] = useState([])
+    const { loaded, portalId } = useToastPortal()
 
     useToastAutoClose({
       toasts,
       setToasts,
       autoClose,
       autoCloseTime,
-    });
+    })
 
     const removeToast = (id) => {
-      setToasts(toasts.filter((t) => t.id !== id));
-    };
+      setToasts(toasts.filter((t) => t.id !== id))
+    }
 
     useImperativeHandle(ref, () => ({
       addMessage(toast) {
-        setToasts([...toasts, { ...toast, id: uuid() }]);
+        setToasts([...toasts, { ...toast, id: uuid() }])
       },
-    }));
+    }))
 
     return loaded ? (
       ReactDOM.createPortal(
@@ -44,6 +44,6 @@ export const ToastPortal = forwardRef(
       )
     ) : (
       <></>
-    );
+    )
   }
-);
+)
